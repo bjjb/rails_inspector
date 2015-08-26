226a1976288386706cf2dd1b0d0fad174b73af42
Dejan Simic <desimic@gmail.com>
2015-08-26 08:51:34 +0200

    Initial commit

---
+++ spec/rails_inspector_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe RailsInspector do
+  it 'has a version number' do
+    expect(RailsInspector::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
